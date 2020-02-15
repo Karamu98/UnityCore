8706aed3be4b09caa2d5d57f62ca3600fb459e87
Karamu98 <callumhaines98@hotmail.co.uk>
2020-02-15 14:58:23 +0000

    Initial push of current Core files

---
+++ Core/UI/Screen.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Core.UI
+{
+    public class Screen : MonoBehaviour
+    {
+        [SerializeField] string m_screenName = default;
+
+        void Awake()
+        {
+            ScreenManager.Get().TrackScreen(m_screenName, this);
+            Init();
+        }
+
+        protected virtual void Init()
+        {
+
+        }
+
+        void OnDestroy()
+        {
+            ScreenManager.Get().StopTrackingScreen(m_screenName);
+        }
+
+        public void SetScreen(string screenToSet)
+        {
+            ScreenManager.Get().SetScreen(screenToSet);
+        }
+    }
+
+    public class ScreenManager
+    {
+        static ScreenManager s_instance;
+        public static ScreenManager Get()
+        {
+            if(s_instance == null)
+            {
+                s_instance = new ScreenManager();
+                s_instance.m_screens = new Dictionary<string, Screen>();
+                s_instance.m_activeScreens = new List<string>();
+            }
+
+            return s_instance;
+        }
+
+        Dictionary<string, Screen> m_screens;
+        List<string> m_activeScreens;
+
+        public void SetScreen(string screenToSet)
+        {
+            foreach(string scrn in m_activeScreens)
+            {
+                m_screens[scrn].gameObject.SetActive(false);
+            }
+
+            m_screens[screenToSet].gameObject.SetActive(true);
+            m_activeScreens.Add(screenToSet);
+        }
+
+        public void EnableScreen(string screenToEnable)
+        {
+            m_screens[screenToEnable].gameObject.SetActive(true);
+            m_activeScreens.Add(screenToEnable);
+        }
+
+        public void TrackScreen(string screenName, Screen newScreen)
+        {
+            m_screens[screenName] = newScreen;
+            m_activeScreens.Add(screenName);
+        }
+
+        public void StopTrackingScreen(string screenName)
+        {
+            if(m_activeScreens.Contains(screenName))
+            {
+                m_activeScreens.Remove(screenName);
+            }
+            m_screens.Remove(screenName);
+        }
+    }
+}
+
---
+++ Core/Utilities/ScreenShotter.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Core.Utilities
+{
+    public class ScreenShotter : MonoBehaviour
+    {
+        static Vector2Int m_ssRes = default;
+        bool takeHiResShot = false;
+
+        Texture2D ScreenshotTexture;
+        RenderTexture _renderTexture;
+
+        public static string ScreenShotName(int width, int height)
+        {
+            return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+                                 Application.dataPath,
+                                 width, height,
+                                 System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+        }
+
+        public void TakeHiResShot()
+        {
+            takeHiResShot = true;
+        }
+
+        private void Awake()
+        {
+            int w = Screen.width;
+            int h = Screen.height;
+            _renderTexture = new RenderTexture(w, h, 0);
+            ScreenshotTexture = new Texture2D(w, h, TextureFormat.RGB24, true);
+        }
+        void LateUpdate()
+        {
+            takeHiResShot |= Input.GetKeyDown("k");
+            if (takeHiResShot)
+            {
+                StartCoroutine(UpdateScreenshotTexture());
+            }
+        }
+
+        public IEnumerator UpdateScreenshotTexture()
+        {
+            yield return new WaitForEndOfFrame();
+            ScreenCapture.CaptureScreenshotIntoRenderTexture(_renderTexture);
+            ScreenshotTexture.ReadPixels(new Rect(0, 0, _renderTexture.width, _renderTexture.height), 0, 0);
+            ScreenshotTexture.Apply();
+
+            byte[] bytes = ScreenshotTexture.EncodeToPNG();
+            string filename = ScreenShotName(m_ssRes.x, m_ssRes.y);
+            if (System.IO.Directory.Exists(Application.dataPath + "/screenshots"))
+            {
+                System.IO.File.WriteAllBytes(filename, bytes);
+                Debug.Log(string.Format("Took screenshot to: {0}", filename));
+                takeHiResShot = false;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(Application.dataPath + "/screenshots");
+                System.IO.File.WriteAllBytes(filename, bytes);
+                Debug.Log(string.Format("Took screenshot to: {0}", filename));
+                takeHiResShot = false;
+            }
+        }
+    }
+}
